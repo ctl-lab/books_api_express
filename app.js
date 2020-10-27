68a68725350342dc19263e853199a5da0f5d56f0
emiliano-ma <emilianomainero@gmail.com>
2020-10-27 16:10:19 +0100

    Creates config.js and app.js. Completing basic express setup for /books to serve the index of books in postgres db.

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { pool } = require("./config");
+
+const app = express();
+
+app
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use(cors());
+
+
+// this is the index action of the books controller
+const getBooks = (request, response) => {
+  pool.query('SELECT * FROM books', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json({books: results.rows})
+  })
+}
+
+// this is the route
+app.route("/books").get(getBooks);
+
+app.listen(process.env.PORT || 3002, () => {
+  console.log("The server is listening...");
+});
