1d92fb5598d6c65ba71144a1b7d14643ce70e870
emiliano-ma <emilianomainero@gmail.com>
2020-10-28 21:22:19 +0100

    Replaces query for body as the source of information in the create action

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const index = (request, response) => {
 };
 
 const create = (request, response) => {
-  const { author, title } = request.query;
+  const { author, title } = request.body;
   pool.query(
     "INSERT INTO books (author, title) VALUES ($1, $2)",
     [author, title],
